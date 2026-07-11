c25177e67a5d9d5cabdb3727e78abfa3907c6189
Beck Harper <beck@fastmail.com>
2026-07-10 22:06:05 -0600

    running on fumes but rewrote initial fetch and added expected types from pokeapi

--- services/pokemon.ts
+++ services/pokemon.ts
@@ -1,9 +1,36 @@
-export async function getPokemon(name: string) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+const POKEAPI_BASE_URL = "https://pokeapi.co/api/v2";
+
+//adding types for the pokeapi response
+export interface Pokemon {
+  id: number;
+  name: string;
+  url: string;
+  image?: string;
+  types?: string[];
+}
+
+export interface PokemonType {
+  name: string;
+  url: string;
+}
+
+export interface PokemonListResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Pokemon[];
+}
+//starting with pokemon
+export async function fetchPokemon(): Promise<Pokemon[]> {
+  const response = await fetch(`${POKEAPI_BASE_URL}/type`, {
+    //forcing local cache here because pokeapi requires it, will change to redis cache later
+    cache: "force-cache",
+  });
 
   if (!response.ok) {
     throw new Error(`pokemon not found: ${response.statusText}`);
   }
+  const data = await response.json();
 
-  return response.json();
+  return data.results.filter((type: Pokemon) => type.name !== "unknown");
 }
