e9658c4ad5b005a0a66c43a2dadd47b0c9e5411d
Beck Harper <beck@fastmail.com>
2026-07-12 07:01:45 -0600

    fell asleep, got map working on api call, created links for individual pokemon pages with more details

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { fetchPokemon } from "@/services/pokemon";
+import { fetchPokemonList, Pokemon } from "@/services/pokemon";
+import Link from "next/link";
 
 const ITEMS_PER_PAGE = 20;
 
@@ -9,16 +10,24 @@ interface HomeProps {
 export default async function Home({ searchParams }: HomeProps) {
   const params = await searchParams;
   const page = parseInt(params.page as string) || 1;
-  const selectedtype = (params.type as string) || "all";
   const offset = (page - 1) * 20;
-
-  const [pokemon] = await Promise.all([fetchPokemon()]);
+  const pokemon = await fetchPokemonList(offset, ITEMS_PER_PAGE);
+  console.log("pokemon", pokemon);
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold mt-25 mb-6 flex flex-col">PokeScry</h1>
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center justify-center gap-4 sm:items-start">
-          hello, world
+        <div className="grid grid-cols-4 gap-4">
+          {pokemon.results.map((p: Pokemon) => (
+            <Link
+              key={p.name}
+              href={`/pokemon/${p.name}`}
+              className="border rounded p-4"
+            >
+              {p.name}
+            </Link>
+          ))}
         </div>
       </main>
     </div>
