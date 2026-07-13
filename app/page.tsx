7eaa064e75db73b04299e6d21f78956d42b2365d
Beck Harper <beck@fastmail.com>
2026-07-12 20:44:02 -0600

    finished reworking homepage, now just a search bar. JSX could be simplified. TODO: better search algo or lib, user account / save pokemon to collection feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,52 @@
 import Link from "next/link";
 import { caller } from "@/server/caller";
-
-const ITEMS_PER_PAGE = 20;
+import SearchBar from "@/components/SearchBar";
 
 interface HomeProps {
-  searchParams: Promise<Record<string, string | string[] | undefined>>;
+  searchParams: Promise<{
+    q?: string;
+  }>;
 }
 
 export default async function Home({ searchParams }: HomeProps) {
   const params = await searchParams;
-  const page = parseInt(params.page as string) || 1;
+  const query = params.q ?? "";
   const api = await caller();
-  const offset = (page - 1) * ITEMS_PER_PAGE;
-  const pokemon = await api.pokemon.list({ offset, limit: ITEMS_PER_PAGE });
+
+  const pokemon = query
+    ? await api.pokemon.search({
+        query,
+      })
+    : [];
 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-4xl font-bold mt-25 mb-6 flex flex-col">PokeScry</h1>
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-col flex-1 items-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold mt-25 mb-6">PokeScry</h1>
+
+      <main className="flex flex-1 w-full max-w-3xl flex-col py-16 px-8 bg-white dark:bg-black">
+        <SearchBar />
+
+        {query && (
+          <h2 className="text-xl mb-4">Search results for `{query}`</h2>
+        )}
+
+        {!query && <p className="text-zinc-500">Search for a pokemon</p>}
+
         <div className="grid grid-cols-4 gap-4">
-          {pokemon.results.map((p: { name: string }) => (
+          {pokemon.map((p) => (
             <Link
               key={p.name}
               href={`/pokemon/${p.name}`}
-              className="border rounded p-4"
+              className="border rounded p-4 capitalize hover:bg-zinc-100 dark:hover:bg-zinc-900"
             >
               {p.name}
             </Link>
           ))}
         </div>
+
+        {query && pokemon.length === 0 && (
+          <p className="text-zinc-500">No Pokémon found.</p>
+        )}
       </main>
     </div>
   );
