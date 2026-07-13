d94df9bd9d1c58c2d9f051f76819b060f363d30c
Beck Harper <beck@fastmail.com>
2026-07-12 20:31:54 -0600

    went with what was just as familiar to me bc it seemed weird to add a single server action after kinda avoiding them

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,3 +1,42 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+export default function SearchBar() {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const [query, setQuery] = useState(params.get("q") ?? "");
+
+  function submitSearch() {
+    const val = query.trim();
+
+    if (!val) {
+      router.push("/");
+      return;
+    }
+    router.push(`/?q=${encodeURIComponent(val)}`);
+  }
+
+  return (
+    <div className="flex gap-2 mb-8">
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            submitSearch();
+          }
+        }}
+        placeholder="Search for pokemon..."
+        className="border rounded px-4 py-2"
+      ></input>
+      <button
+        onClick={submitSearch}
+        className="border rounded px-4 py-2"
+      ></button>{" "}
+      Search
+    </div>
+  );
+}
