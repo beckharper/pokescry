1c6760ee04a07aa89926a35277cf2758c119d442
Beck Harper <beck@fastmail.com>
2026-07-13 17:38:32 -0600

    starting more ui customization

--- app/pokemon/[name]/page.tsx
+++ app/pokemon/[name]/page.tsx
@@ -12,14 +12,14 @@ export default async function PokemonPage({ params }: PokemonPageProps) {
   const pokemon = await api.pokemon.byName({ name });
 
   return (
-    <main className="max-w-3xl mx-auto p-8">
-      <h1 className="text-5xl capitalize *:font-bold mb-6">{pokemon.name}</h1>
+    <main className="max-w-3xl mx-auto p-8 ">
+      <h1 className="text-5xl capitalize *:font-bold mb-6 ">{pokemon.name}</h1>
       <Image
         src={pokemon.sprites.front_default ?? "/placeholder.png"}
         alt={pokemon.name}
         width={200}
         height={200}
-        className="w-64 h-64 mb-6"
+        className="w-64 h-64 mb-6 items-center mt-45"
       />
       <p>Height: {pokemon.height}</p>
       <p> Weight: {pokemon.weight}</p>
